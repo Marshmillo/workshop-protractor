7d5e8a774073c8b7fd98a4fb8f03baab1a2a231a
Marshmillo <johancamilodiaz@gmail.com>
2019-10-04 08:30:02 -0500

    Identación mejorada - Lint agregado

--- test/google.spec.ts
+++ test/google.spec.ts
@@ -1,13 +1,13 @@
 import { browser } from 'protractor';
 
 describe('Given a SDET learning protractor', () => {
-	describe ('when open Google Page', () => {
-		beforeEach(async () => {
-		browser.get('https://www.google.com');
-		});
+  describe('when open Google Page', () => {
+    beforeEach(async () => {
+      browser.get('https://www.google.com');
+    });
 
-		it('then should have a title', async () =>{
-		expect(browser.getTitle()).toEqual('Google');
-		});
-	});
+    it('then should have a title', async () => {
+      expect(browser.getTitle()).toEqual('Google');
+    });
+  });
 });
